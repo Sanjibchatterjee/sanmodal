7ecf29900c06916f4ab9ba824de41652e3b6649f
Sanjib Chatterjee <Sanjibchatterjee@users.noreply.github.com>
2018-06-17 12:43:28 +0530

    Add files via upload

---
+++ sanmodal.js
@@ -0,0 +1,4 @@
+/* ----opyright @Sanjib Chatterjee----
+You are hereby authorized to use this modal till you keep this header and copyright information and till then it is:
+Free for Personal & Commercial Projects */
+btn.onclick=function(){modal.style.display="block"},close.onclick=function(){modal.style.display="none"},btnbot.onclick=function(){modalbot.style.display="block"},closebot.onclick=function(){modalbot.style.display="none"},window.onclick=function(o){o.target!=modal&&o.target!=modalbot||(modal.style.display="none",modalbot.style.display="none")};
